96ec9e428f3c2203a5d9d2b93461204c6867159c
Jess Hause Ugray <jess.ugray@gmail.com>
2022-01-07 13:16:33 -0500

    about section

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav';
+import About from './components/About'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <main>
+      <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import profilePic from "../../assets/profilepic.png";
+
+function About() {
+  return (
+    <section>
+      <h1>Who Am I?</h1>
+      <p>Hi! I'm Jess, a developer from Ottawa, Canada. I have a passion for programming.</p>
+      <img src={profilePic}></img>
+    </section>
+  )
+}
+
+export default About
