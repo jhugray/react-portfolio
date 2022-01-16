5f22ad723cd277c02e86043c3088722861aca04f
Jess Hause Ugray <jess.ugray@gmail.com>
2022-01-16 09:38:52 -0500

    updated style

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,80 +6,134 @@ import { Card, Row, Col, Button } from 'react-bootstrap';
 function Portfolio() {
 
   return (
-    <Row id="portfolio" xs={1} md={2} className="g-4">
-     
+    <Row id="portfolio" xs={1} md={2} className="g-4 mt-2">
+
       <Col>
-      
-      
-    <Card className="mb-3 text-center">
-      <Card.Img target="_blank" src="https://user-images.githubusercontent.com/59127869/144726238-13142112-1a62-4314-a4e4-a48fee64c4d7.png" /> 
-      <Card.Body>
-      <Card.Title>Sputtle</Card.Title>
-      <Card.Text>
-        This is an app that does ... 
-      </Card.Text>
-      <Button variant="primary" href="https://music-battle-spotify.herokuapp.com/" target="_blank">Check it out!</Button>
-      </Card.Body>
-    </Card>
-
-    </Col>
-    <Col>
-
-    <Card className="mb-3 text-center">
-      <Card.Img target="_blank" src="https://user-images.githubusercontent.com/59127869/144726238-13142112-1a62-4314-a4e4-a48fee64c4d7.png" /> 
-      <Card.Body>
-        <Card.Title>Sputtle</Card.Title>
-        <Card.Text>
-        This is an app that does ... 
-        </Card.Text>
-        <Button variant="primary"  href="https://music-battle-spotify.herokuapp.com/" target="_blank">Check it out!</Button>
-      </Card.Body>
-      
-    </Card>
-</Col>
-<Col>
-    <Card className="mb-3 text-center">
-      <Card.Img target="_blank" src="https://user-images.githubusercontent.com/59127869/144726238-13142112-1a62-4314-a4e4-a48fee64c4d7.png" /> 
-      <Card.Title>Sputtle</Card.Title>
-      <Card.Text>
-        This is an app that does ... 
-      </Card.Text>
-    </Card>
-
+        <Card className="mb-3 text-center">
+          <Card.Img target="_blank" src="https://user-images.githubusercontent.com/59127869/144726238-13142112-1a62-4314-a4e4-a48fee64c4d7.png" /> 
+          <Card.Body>
+            <Card.Title>Sputtle</Card.Title>
+            <Card.Link href="https://github.com/jhugray/music-battle-spotify">https://github.com/jhugray/music-battle-spotify</Card.Link>
+            <br></br>
+            <Card.Text>
+            <p>
+            Sputtle uses the Spotify API to battle songs against each other to see who will rise to the top of the leaderboard. It’s a fun way to listen to music and discover new songs.
+            </p>
+            <p>
+              Login: sputtle.user@gmail.com 
+            <br></br>
+              Password: sputtle-demo-password
+            </p>
+            </Card.Text>
+        
+            <Button variant="primary"  href="https://music-battle-spotify.herokuapp.com/" target="_blank">Check it out!</Button>
+          </Card.Body>
+        </Card>
       </Col>
-     
 
+      <Col>    
+        <Card className="mb-3 text-center">
+          <Card.Img target="_blank" src="https://user-images.githubusercontent.com/59127869/135762566-01811582-1867-49da-846b-8afa70a7f11c.png" /> 
+          <Card.Body>
+          <Card.Title>Weather Dashboard</Card.Title>
+          <Card.Link href="https://github.com/jhugray/weatherdashboard">https://github.com/jhugray/weatherdashboard</Card.Link>
+            <br></br>
+          <Card.Text>
+            <p>
+              This is a user friendly interface where you can easily look up weather by city. Your recent searches are saved in quick access buttons to easily flip between your common locations. 
+            </p>
+          </Card.Text>
+          <Button variant="primary" href="https://jhugray.github.io/weatherdashboard/" target="_blank">Check it out!</Button>
+          </Card.Body>
+        </Card>
+      </Col>
 
-  
-    {/* <a href="https://music-battle-spotify.herokuapp.com/" target="_blank" class="project-main sputtle">
-      <h4>Sputtle</h4>
-    </a>
+      <Col>    
+        <Card className="mb-3 text-center">
+          <Card.Img target="_blank" src="https://user-images.githubusercontent.com/59127869/143130090-1784fc09-ffc6-453b-9800-c896e3ef88e2.png" /> 
+          <Card.Body>
+          <Card.Title>MVC (Model-View-Controller) Blog</Card.Title>
+          <Card.Link href="https://github.com/jhugray/mvc-tech-blog">https://github.com/jhugray/mvc-tech-blog</Card.Link>
+            <br></br>
+          <Card.Text>
+            <p>
+            A tech blog for sharing all your thoughts and ideas. Users can log in to post. Set up using an MVC pattern.            
+            </p>
+          </Card.Text>
+          <Button variant="primary" href="https://mvc-tech-blog-by-jhu.herokuapp.com/" target="_blank">Check it out!</Button>
+          </Card.Body>
+        </Card>
+      </Col>
 
-    <a href="https://jhugray.github.io/run-buddy/" target="_blank" class="project-ex runbuddy">
-      <h4>Run Buddy</h4>
-    </a>
+      <Col>    
+        <Card className="mb-3 text-center">
+          <Card.Img target="_blank" src="https://user-images.githubusercontent.com/59127869/147973811-45c6c7cd-07da-420e-868d-5ea175897c52.png" /> 
+          <Card.Body>
+          <Card.Title>Budget Progressive Web App</Card.Title>
+          <Card.Link href="https://github.com/jhugray/budgetPWA">https://github.com/jhugray/budgetPWA</Card.Link>
+            <br></br>
+          <Card.Text>
+            <p>
+              A PWA to manage your budget, online or offline!
+            </p>
+          </Card.Text>
+          <Button variant="primary" href="https://a19budget-tracker-pwa.herokuapp.com/" target="_blank">Check it out!</Button>
+          </Card.Body>
+        </Card>
+      </Col>
 
+      <Col>    
+        <Card className="mb-3 text-center">
+          <Card.Img target="_blank" src="https://user-images.githubusercontent.com/59127869/140653265-0366ee2c-acee-4665-afe8-be5df72c93b9.png" /> 
+          <Card.Body>
+          <Card.Title>Note Taker</Card.Title>
+          <Card.Link href="https://github.com/jhugray/note-tracker">https://github.com/jhugray/note-tracker</Card.Link>
+            <br></br>
+          <Card.Text>
+            <p>
+              I designed the backend using express for this note taker app. 
+            </p>
+          </Card.Text>
+          <Button variant="primary" href="https://note-tracker-by-jess.herokuapp.com/" target="_blank">Check it out!</Button>
+          </Card.Body>
+        </Card>
+      </Col>
 
-    <a href="https://jhugray.github.io/horiseon/" target="_blank" class="project-ex horiseon">
-     <h4>Horiseon</h4> 
-    </a>
+      <Col>    
+        <Card className="mb-3 text-center">
+          <Card.Img target="_blank" src="https://user-images.githubusercontent.com/59127869/133896933-1b60f67d-b6a4-4b5d-88fe-e5b38bb4b905.png" /> 
+          <Card.Body>
+          <Card.Title>Code Quiz</Card.Title>
+          <Card.Link href="https://github.com/jhugray/code-quiz">https://github.com/jhugray/code-quiz</Card.Link>
+            <br></br>
+          <Card.Text>
+            <p>
+              A fun quiz game to test your coding knowledge. 
+            </p>
+          </Card.Text>
+          <Button variant="primary" href="https://jhugray.github.io/code-quiz/" target="_blank">Check it out!</Button>
+          </Card.Body>
+        </Card>
+      </Col>
 
-    <a href="https://jhugray.github.io/weatherdashboard/" target="_blank" class="project-ex weatherDashboard">
-      <h4>Weather<br />Dashboard</h4>
-    </a>
+      <Col>    
+        <Card className="mb-3 text-center">
+          <Card.Img target="_blank" src="https://user-images.githubusercontent.com/59127869/138617343-31729736-84c1-47e9-8044-1c1bf4640176.png" /> 
+          <Card.Body>
+          <Card.Title>ReadMe Generator</Card.Title>
+          <Card.Link href="https://github.com/jhugray/README-generator">https://github.com/jhugray/README-generator</Card.Link>
+            <br></br>
+          <Card.Text>
+            <p>
+              Easily generate Readme's from the command line using this convenient app. 
+            </p>
+          </Card.Text>
+          <Button variant="primary" href="https://github.com/jhugray/README-generator" target="_blank">Check it out!</Button>
+          </Card.Body>
+        </Card>
+      </Col>
 
-    <a href="https://github.com/azuryte5/Nutrition-Sustenance-Value" target="_blank" class="project-ex nutrivalue">
-      <h4>Nutrivalue</h4>
-    </a>
-
-    <a href="" target="_blank" class="project-ex project5">
-      <h4>Project 5</h4>
-    </a>
-
-    <a href="" target="_blank" class="project-ex project5">
-      <h4>Project 5</h4>
-    </a> */}
-
+    
 
   </Row>
   )
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,7 +7,7 @@ function Resume() {
     <Container>    
       <Row className="mt-5 m-4 mb-0">
         <Col> 
-        <Button className="m-2" href="https://docs.google.com/document/d/e/2PACX-1vS3q53FrjJN1HFwznNgPTDQvlHBKocuTYvKZ8nF1-s2i1apmY-FpEPQVho0dzG5zJE3Rzd1ndqXBeQO/pub?embedded=true" target="_blank">
+        <Button className="m-2" href="https://docs.google.com/document/d/e/2PACX-1vS3q53FrjJN1HFwznNgPTDQvlHBKocuTYvKZ8nF1-s2i1apmY-FpEPQVho0dzG5zJE3Rzd1ndqXBeQO/pub" target="_blank">
           <h2>Open my resume in a new window</h2>
           <p>You can view it in a larger window and right click to save it!</p>
         </Button>
@@ -16,7 +16,7 @@ function Resume() {
       </Row>
       <Row style={{ width: 'auto', height: 'auto' }}>
         <Ratio>
-          <iframe id="resume" src="https://docs.google.com/document/d/e/2PACX-1vS3q53FrjJN1HFwznNgPTDQvlHBKocuTYvKZ8nF1-s2i1apmY-FpEPQVho0dzG5zJE3Rzd1ndqXBeQO/pub?embedded=true"></iframe>
+          <iframe id="resume" src="https://docs.google.com/document/d/e/2PACX-1vS3q53FrjJN1HFwznNgPTDQvlHBKocuTYvKZ8nF1-s2i1apmY-FpEPQVho0dzG5zJE3Rzd1ndqXBeQO/pub"></iframe>
         </Ratio>
       </Row>
     </Container>
