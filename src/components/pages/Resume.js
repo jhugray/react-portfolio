632a08659cd93838c7c7645fd5e1b7ad77752479
Jess Hause Ugray <jess.ugray@gmail.com>
2022-04-26 09:36:40 -0400

    resume links updated

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,7 +7,7 @@ function Resume() {
     <Container>    
       <Row className="mt-5 m-4 mb-0">
         <Col> 
-        <Button className="m-2" href="https://docs.google.com/document/d/e/2PACX-1vS3q53FrjJN1HFwznNgPTDQvlHBKocuTYvKZ8nF1-s2i1apmY-FpEPQVho0dzG5zJE3Rzd1ndqXBeQO/pub" target="_blank">
+        <Button className="m-2" href="https://docs.google.com/document/d/e/2PACX-1vQXRNA4Al0iY9E4UeTS9W86shXW-u3QUFbGxFnoeayF0vLymm-4-cKRKw1SQfDqDm2YY1LVPIsJjIkD/pub" target="_blank">
           <h2>Open my resume in a new window</h2>
           <p>You can view it in a larger window and right click to save it!</p>
         </Button>
@@ -16,7 +16,7 @@ function Resume() {
       </Row>
       <Row style={{ width: 'auto', height: 'auto' }}>
         <Ratio>
-          <iframe id="resume" src="https://docs.google.com/document/d/e/2PACX-1vQnMwFyyNvG1A4Isp9IzTL5EzwaqnJ_xUhl-3akPMn0BdyjUiygAAg9irTLCK3cyOpiuAEDj-mnjXhb/pub"></iframe>
+          <iframe id="resume" src="https://docs.google.com/document/d/e/2PACX-1vQXRNA4Al0iY9E4UeTS9W86shXW-u3QUFbGxFnoeayF0vLymm-4-cKRKw1SQfDqDm2YY1LVPIsJjIkD/pub"></iframe>
         </Ratio>
       </Row>
     </Container>
